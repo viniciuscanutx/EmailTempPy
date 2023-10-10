9b758c80cd976ced737675267c664af257980382
viniciuscanutx <viniciuscanutmz@gmail.com>
2023-10-10 12:53:21 -0300

    Modificando o arquivo main e criando novos diretorios pro projeto

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+from tkinter import messagebox
 from mailtm import Email
 import PySimpleGUI as sg
+import pyperclip
 
 class EmailTemp:
     def __init__(self):
@@ -7,19 +9,20 @@ class EmailTemp:
         layout = [
             [sg.Text('Seu Email Temporário: ',size=(17, 1)), sg.Output(size=(40, 1), key='output1'), sg.Button('Copiar!')],
             [sg.Output(size=(69, 10), key='output2')],
-            [sg.Text('Para gerar um outro email, reinicie o programa.',size=(35, 1))],
-            [sg.Button('Iniciar')]
+            [sg.Text('Clique em gerar para gerar um novo email.',size=(35, 1))],
+            [sg.Button('Gerar')]
 
         ]
 
-        self.janela = sg.Window('Temporary Email', layout, icon=f'./img/ico1.ico')
+        self.janela = sg.Window('Temporary Email', layout, icon=f'./img/icon1.ico')
 
     def Iniciar(self):
+        global emailfield
         while True:
             evento, valores = self.janela.read()
             if evento == sg.WINDOW_CLOSED:
                 break
-            if evento == 'Iniciar':
+            if evento == 'Gerar':
                 test = Email()
                 test.register()
                 emailfield = test.address
@@ -27,11 +30,15 @@ class EmailTemp:
                 self.janela['output1'].update(emailfield)
                 self.janela['output2'].update('')
                 print("(Seus Emails Aparecerão abaixo)\nEsperando novos emails...")
+            if evento == 'Copiar!':
+                pyperclip.copy(emailfield)
+
 
     def listener(self, message):
         print('\nNovo Email: ')
         print("\nAssunto: " + message['subject'])
         print("Conteúdo:  " + message['text'] if message['text'] else message['html'])
+
 
 gen = EmailTemp()
 gen.Iniciar()
