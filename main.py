e2f16d1b60eacded3486a8937371c6f5727e9cfe
viniciuscanutx <viniciuscanutmz@gmail.com>
2023-10-10 13:55:32 -0300

    Projeto TempEmail finalizado

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import time
 from tkinter import messagebox
 from mailtm import Email
 import PySimpleGUI as sg
@@ -7,20 +8,23 @@ class EmailTemp:
     def __init__(self):
         sg.theme('Reddit')
         layout = [
+            [sg.Text('Clique em gerar para gerar um novo email.',size=(35, 1))],
             [sg.Text('Seu Email Temporário: ',size=(17, 1)), sg.Output(size=(40, 1), key='output1'), sg.Button('Copiar!')],
             [sg.Output(size=(69, 10), key='output2')],
-            [sg.Text('Clique em gerar para gerar um novo email.',size=(35, 1))],
             [sg.Button('Gerar')]
 
         ]
 
         self.janela = sg.Window('Temporary Email', layout, icon=f'./img/icon1.ico')
 
+
+
     def Iniciar(self):
         global emailfield
         while True:
             evento, valores = self.janela.read()
             if evento == sg.WINDOW_CLOSED:
+                test.stop( )
                 break
             if evento == 'Gerar':
                 test = Email()
@@ -29,9 +33,10 @@ class EmailTemp:
                 test.start(self.listener)
                 self.janela['output1'].update(emailfield)
                 self.janela['output2'].update('')
-                print("(Seus Emails Aparecerão abaixo)\nEsperando novos emails...")
+                print("\nEsperando novos emails...")
             if evento == 'Copiar!':
                 pyperclip.copy(emailfield)
+                self.janela['output2'].update('Copiado com Sucesso!\nEsperando novos emails...')
 
 
     def listener(self, message):
