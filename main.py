3b65f509f43202375d752188332666082046aa12
viniciuscanutx <viniciuscanutmz@gmail.com>
2023-10-10 12:17:18 -0300

    Modificando o arquivo main e criando novos diretorios pro projeto

--- main.py
+++ main.py
@@ -1,23 +1,37 @@
-import requests
 from mailtm import Email
+import PySimpleGUI as sg
 
-def listener(message):
-    print("\nAssunto: " + message['subject'])
-    print("Conteúdo:  " + message['text'] if message['text'] else message['html'])
+class EmailTemp:
+    def __init__(self):
+        sg.theme('Reddit')
+        layout = [
+            [sg.Text('Seu Email Temporário: ',size=(17, 1)), sg.Output(size=(40, 1), key='output1'), sg.Button('Copiar!')],
+            [sg.Output(size=(69, 10), key='output2')],
+            [sg.Text('Para gerar um outro email, reinicie o programa.',size=(35, 1))],
+            [sg.Button('Iniciar')]
 
-def mainclass():
-    name = input("Insira seu nome: ")
-    if name == "":
-        print("Por Favor, Insira seu nome.\n")
-        return mainclass()
-    else:
-        print(f"Bem vindo, {name}")
-        test = Email()
-        print("\nDomínio do seu email: " + test.domain)
-        test.register()
-        print(f"\n{name}, Seu Email Temporário é: " + str(test.address))
-        test.start(listener)
-        print("\nEsperando novos emails...\n(Seus Emails Aparecerão abaixo)")
+        ]
 
+        self.janela = sg.Window('Temporary Email', layout, icon=f'./img/ico1.ico')
 
-mainclass()
+    def Iniciar(self):
+        while True:
+            evento, valores = self.janela.read()
+            if evento == sg.WINDOW_CLOSED:
+                break
+            if evento == 'Iniciar':
+                test = Email()
+                test.register()
+                emailfield = test.address
+                test.start(self.listener)
+                self.janela['output1'].update(emailfield)
+                self.janela['output2'].update('')
+                print("(Seus Emails Aparecerão abaixo)\nEsperando novos emails...")
+
+    def listener(self, message):
+        print('\nNovo Email: ')
+        print("\nAssunto: " + message['subject'])
+        print("Conteúdo:  " + message['text'] if message['text'] else message['html'])
+
+gen = EmailTemp()
+gen.Iniciar()
